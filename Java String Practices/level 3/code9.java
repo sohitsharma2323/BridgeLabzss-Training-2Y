21b2e1a402c9e3bc37487bc534b9d07c98083533
sohitsharma2323 <sohitsharma2323@gmail.com>
2025-08-31 19:59:23 +0530

    Update and rename java9 (2).java to code9.java

--- Java String Practices/level 3/code9.java
+++ Java String Practices/level 3/code9.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class java9 {
+class code9 {
     public static boolean isLeapYear(int year) {
         return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
