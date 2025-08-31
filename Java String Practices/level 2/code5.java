01d62c08b3b7e31ffd042b78fc48960c56d9412c
sohitsharma2323 <sohitsharma2323@gmail.com>
2025-08-31 19:47:21 +0530

    Update and rename java5 (1).java to code5.java

--- Java String Practices/level 2/code5.java
+++ Java String Practices/level 2/code5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class java5 {
+class code5 {
     public static boolean isVowel(char ch) {
         ch = Character.toLowerCase(ch);
         return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
