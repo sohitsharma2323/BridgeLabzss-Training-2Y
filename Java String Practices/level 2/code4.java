aec0c076e388505e76ea2cbc6b59b0c98fa88e43
sohitsharma2323 <sohitsharma2323@gmail.com>
2025-08-31 19:46:48 +0530

    Update and rename java4 (1).java to code4.java

--- Java String Practices/level 2/code4.java
+++ Java String Practices/level 2/code4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class java4 {
+class code4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a sentence: ");
