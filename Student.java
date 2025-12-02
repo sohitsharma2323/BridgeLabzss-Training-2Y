c2472be554a1e6556bb903bb55242b02a89666e1
sohitsharma2323 <sohitsharma2323@gmail.com>
2025-12-02 11:24:36 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    private int studentId;
+    private String name;
+    private String course;
+
+    public Student(int studentId, String name, String course) {
+        this.studentId = studentId;
+        this.name = name;
+        this.course = course;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCourse() {
+        return course;
+    }
+}
